d0c6fc4c6c8c2775b8ed81e47b21fa9b1c6eba0d
Pav0l <pavol.karas@gmail.com>
2019-07-29 18:10:40 +0200

    Update DB, add root endpoint, update ENVs

--- hacker_hunt/hacker_hunt.py
+++ hacker_hunt/hacker_hunt.py
@@ -3,35 +3,49 @@ import requests
 
 from flask import Flask, jsonify, request
 from utils import consts
-from mongo import find_any, update_visited
+from mongo import Database
+from settings import TOKEN, DB, DB_NAME
+
 
 class FlaskTest():
     def get_init(self):
-        res = requests.get(f"{consts['path']}{consts['init']}", headers={'Authorization': f"Token {os.environ['TOKEN']}"})
-        return res.json()
-    
-    def get_status(self):
-        res = requests.post(f"{consts['path']}{consts['status']}", headers={'Authorization': f"Token {os.environ['TOKEN']}"})
+        res = requests.get(f"{consts['path']}{consts['init']}", headers={
+                           'Authorization': f"Token {TOKEN}"})
         return res.json()
 
+    def get_status(self):
+        res = requests.post(f"{consts['path']}{consts['status']}", headers={
+                            'Authorization': f"Token {TOKEN}"})
+        return res.json()
+
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def server_check():
+    return 'Server is running'
+
 
 @app.route('/launch')
 def launch_app():
     '''Initiates the application, the main logic loop goes inside here'''
     FT = FlaskTest()
-
-    if not find_any():
-        db_id = insert_to_db()
-    else:
-        a = find_any()
-        db_id = a["_id"]
+    db = Database(os.environ['DB'], os.environ['DB_NAME'])
+    db_id = db.get_id()
 
     # get init
     init = FT.get_init()
-    update_visited(db_id, init["room_id"])
+    print(f"Number of players: {len(init['players'])}")
+    # test db connection
+    db.update_visited(db_id, init["room_id"])
+
     return f"{init}"
 
     # get status
     # status = FT.get_status()
     # return f"{status}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
