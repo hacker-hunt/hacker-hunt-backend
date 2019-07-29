fd994d18761ee4b8bd2242fd82520aff80ca42b4
Pav0l <pavol.karas@gmail.com>
2019-07-29 21:13:26 +0200

    Take out get_status to init Player instance

--- hacker_hunt/player.py
+++ hacker_hunt/player.py
@@ -3,8 +3,9 @@ import requests
 from settings import TOKEN
 from utils import consts
 
+
 class Player:
-    def __init__(self, p, room):
+    def __init__(self, p):
         '''Construct a new Player class'''
         self.name = p["name"]
         self.cooldown = p["cooldown"]
@@ -16,8 +17,7 @@ class Player:
         self.status = p["status"]
         self.errors = p["errors"]
         self.messages = p["messages"]
-        # this does not come from the player dict
-        self.current_room = room
+
         # auth token for requests
         self.token = TOKEN
         self.auth = {"Authorization": f"Token {self.token}"}
@@ -25,51 +25,52 @@ class Player:
     def initalize(self):
         '''Get all relevant stats before player starts moving.'''
         res = requests.get(
-            f"{consts['path']}{consts['init']}", 
+            f"{consts['path']}{consts['init']}",
             headers=self.auth
         )
         return res.json()
-    
+
     def move(self, direction):
         '''Move the player in a provided direction.'''
         res = requests.post(
-            f"{consts['path']}{consts['move']}", 
+            f"{consts['path']}{consts['move']}",
             headers=self.auth,
             json={"direction": f"{direction}"}
         )
         return res.json()
-    
+
     def wise_explore(self, direction, next_room_id):
         '''Move the player to a known room for 50% faster cooldown.'''
         res = requests.post(
-            f"{consts['path']}{consts['move']}", 
+            f"{consts['path']}{consts['move']}",
             headers=self.auth,
-            json={"direction": f"{direction}", "next_room_id": f"{next_room_id}"}
+            json={"direction": f"{direction}",
+                  "next_room_id": f"{next_room_id}"}
         )
         return res.json()
-    
+
     def take_item(self, item_name="treasure"):
         '''Pick up an item found in a room.'''
         res = requests.post(
-            f"{consts['path']}{consts['take']}", 
+            f"{consts['path']}{consts['take']}",
             headers=self.auth,
             json={"name": f"{item_name}"}
         )
         return res.json()
-    
+
     def drop_item(self, item_name="treasure"):
         '''Drop an item held in inventory.'''
         res = requests.post(
-            f"{consts['path']}{consts['drop']}", 
+            f"{consts['path']}{consts['drop']}",
             headers=self.auth,
             json={"name": f"{item_name}"}
         )
         return res.json()
-    
+
     def sell_item(self, item_name="treasure"):
         '''Sell an item held in inventory at a shop.'''
         res = requests.post(
-            f"{consts['path']}{consts['sell']}", 
+            f"{consts['path']}{consts['sell']}",
             headers=self.auth,
             json={"name": f"{item_name}", 'confirm': 'yes'}
         )
@@ -78,7 +79,7 @@ class Player:
     def examine_item(self, item_name):
         '''Examine items in inventory'''
         res = requests.post(
-            f"{consts['path']}{consts['examine']}", 
+            f"{consts['path']}{consts['examine']}",
             headers=self.auth,
             json={"name": f"{item_name}"}
         )
@@ -86,49 +87,51 @@ class Player:
     def examine_player(self, player_name):
         '''Examine player in the same room'''
         res = requests.post(
-            f"{consts['path']}{consts['examine']}", 
+            f"{consts['path']}{consts['examine']}",
             headers=self.auth,
             json={"name": f"{player_name}"}
         )
-    
-    def get_status(self):
-        '''Check status and inventory.'''
-        res = requests.post(
-            f"{consts['path']}{consts['status']}", 
-            headers=self.auth
-        )
-        return res.json()
-    
+
     def change_name(self, new_name):
         '''Change player name at a name changer room'''
         res = requests.post(
-            f"{consts['path']}{consts['change_name']}", 
+            f"{consts['path']}{consts['change_name']}",
             headers=self.auth,
             json={"name": f"{new_name}"}
         )
-    
+
     def pray(self):
         '''Pray at a shrine to earn powers'''
         res = requests.post(
-            f"{consts['path']}{consts['pray']}", 
+            f"{consts['path']}{consts['pray']}",
             headers=self.auth
         )
-    
+
     def fly(self, direction):
         '''Use the flight power to move without penalty on elevated terrain'''
         res = requests.post(
-            f"{consts['path']}{consts['fly']}", 
+            f"{consts['path']}{consts['fly']}",
             headers=self.auth,
             json={"direction": f"{direction}"}
         )
         return res.json()
-    
+
     def dash(self, next_rooms_list):
         '''Cover many rooms in one direction quickly'''
         next_room_ids = ",".join([str(item) for item in next_rooms_list])
         res = requests.post(
-            f"{consts['path']}{consts['fly']}", 
+            f"{consts['path']}{consts['fly']}",
             headers=self.auth,
             json={"direction": f"{next_room_ids}"}
         )
         return res.json()
+
+
+# Use this function to initialize an instance of player
+def get_status():
+    '''Get basic player status and inventory.'''
+    res = requests.post(
+        f"{consts['path']}{consts['status']}",
+        headers={"Authorization": f"Token {TOKEN}"}
+    )
+    return res.json()
