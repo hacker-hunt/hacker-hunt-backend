ab284e6c71d7cf80a55a6dc1a4e003c9ddbff66c
mark-marshall <mark.marshallgp@gmail.com>
2019-07-29 17:32:25 +0100

    Added rest of server requests

--- hacker_hunt/hacker_hunt.py
+++ hacker_hunt/hacker_hunt.py
@@ -5,14 +5,124 @@ from flask import Flask, jsonify, request
 from utils import consts
 from mongo import find_any, update_visited
 
-class FlaskTest():
-    def get_init(self):
-        res = requests.get(f"{consts['path']}{consts['init']}", headers={'Authorization': f"Token {os.environ['TOKEN']}"})
+class ServerRequests():
+    '''Construct a new SR class'''
+    def __init__(self):
+        self.token = os.environ['TOKEN']
+        self.authorization = {"Authorization": f"{self.token}"}
+        
+
+    def initalize(self):
+        '''Get all relevant stats before player starts moving.'''
+        res = requests.get(
+            f"{consts['path']}{consts['init']}", 
+            headers=self.authorization
+        )
         return res.json()
     
-    def get_status(self):
-        res = requests.post(f"{consts['path']}{consts['status']}", headers={'Authorization': f"Token {os.environ['TOKEN']}"})
+    def move(self, direction):
+        '''Move the player in a provided direction.'''
+        res = requests.post(
+            f"{consts['path']}{consts['move']}", 
+            headers=self.authorization,
+            json={"direction": f"{direction}"}
+        )
         return res.json()
+    
+    def wise_explorer(self, direction, next_room_id):
+        '''Move the player to a known room for 50% faster cooldown.'''
+        res = requests.post(
+            f"{consts['path']}{consts['move']}", 
+            headers=self.authorization,
+            json={"direction": f"{direction}", "next_room_id": f"{next_room_id}"}
+        )
+        return res.json()
+    
+    def take_item(self, item_name="treasure"):
+        '''Pick up an item found in a room.'''
+        res = requests.post(
+            f"{consts['path']}{consts['take']}", 
+            headers=self.authorization,
+            json={"name": f"{item_name}"}
+        )
+        return res.json()
+    
+    def drop_item(self, item_name="treasure"):
+        '''Drop an item held in inventory.'''
+        res = requests.post(
+            f"{consts['path']}{consts['drop']}", 
+            headers=self.authorization,
+            json={"name": f"{item_name}"}
+        )
+        return res.json()
+    
+    def sell_item(self, item_name="treasure"):
+        '''Sell an item held in inventory at a shop.'''
+        res = requests.post(
+            f"{consts['path']}{consts['sell']}", 
+            headers=self.authorization,
+            json={"name": f"{item_name}", 'confirm': 'yes'}
+        )
+        return res.json()
+
+    def examine_item(self, item_name):
+        '''Examine items in inventory'''
+        res = requests.post(
+            f"{consts['path']}{consts['examine']}", 
+            headers=self.authorization,
+            json={"name": f"{item_name}"}
+        )
+
+    def examine_player(self, player_name):
+        '''Examine player in the same room'''
+        res = requests.post(
+            f"{consts['path']}{consts['examine']}", 
+            headers=self.authorization,
+            json={"name": f"{player_name}"}
+        )
+    
+    def status(self):
+        '''Check status and inventory.'''
+        res = requests.post(
+            f"{consts['path']}{consts['status']}", 
+            headers=self.authorization
+        )
+        return res.json()
+    
+    def change_name(self, new_name):
+        '''Change player name at a name changer room'''
+        res = requests.post(
+            f"{consts['path']}{consts['change_name']}", 
+            headers=self.authorization,
+            json={"name": f"{new_name}"}
+        )
+    
+    def pray_at_shrine(self):
+        '''Pray at a shrine to earn powers'''
+        res = requests.post(
+            f"{consts['path']}{consts['pray']}", 
+            headers=self.authorization
+        )
+    
+    def fly(self, direction):
+        '''Use the flight power to move without penalty on elevated terrain'''
+        res = requests.post(
+            f"{consts['path']}{consts['fly']}", 
+            headers=self.authorization,
+            json={"direction": f"{direction}"}
+        )
+        return res.json()
+    
+    def dash(self, next_rooms_list):
+        '''Cover many rooms in one direction quickly'''
+        next_room_ids = ",".join([str(item) for item in next_rooms_list])
+        res = requests.post(
+            f"{consts['path']}{consts['fly']}", 
+            headers=self.authorization,
+            json={"direction": f"{next_room_ids}"}
+        )
+        return res.json()
+    
 
 app = Flask(__name__)
 
@@ -28,10 +138,13 @@ def launch_app():
         db_id = a["_id"]
 
     # get init
-    init = FT.get_init()
+    init = FT.initalize()
     update_visited(db_id, init["room_id"])
     return f"{init}"
 
     # get status
-    # status = FT.get_status()
+    # status = FT.status()
     # return f"{status}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
