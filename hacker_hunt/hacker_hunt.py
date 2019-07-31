f9eff3d62f051c071867053861ec601a76cfaf25
Pav0l <pavol.karas@gmail.com>
2019-07-31 10:03:12 +0200

    Setup for 2 player tests

--- hacker_hunt/hacker_hunt.py
+++ hacker_hunt/hacker_hunt.py
@@ -4,7 +4,7 @@ from flask import Flask
 from mongo import Database
 from player import Player, get_status
 from test_obj import test_obj
-from settings import DB, DB_NAME
+from settings import DB, DB_NAME, PROD_DB
 from algo import explore
 
 
@@ -13,20 +13,35 @@ app = Flask(__name__)
 p = Player(get_status())
 db = Database(DB, DB_NAME)
 db_id = db.get_id()
+atlas_db = Database(PROD_DB, DB_NAME)
+atlas_id = atlas_db.get_id()
+
+# print(db_id, atlas_id)
+# data_to_upload = db.download_data(db_id)
+# print(data_to_upload)
+# print(atlas_db.upload_data_to_atlas(data_to_upload))
+
+# rooms_to_upload = list(db.download_rooms())
+# print(rooms_to_upload)
+# print(atlas_db.upload_rooms_to_atlas(rooms_to_upload))
 
 # Manual commands test
 # print(p.initalize())
 
 # print(p.dash('w', '7', '327,256,243,178,90,86,80'))
-# print(f"{p.move('e')}")
-# print(f"{p.wise_explore('e', 0)}")
-# print(get_status())
-# print(p.sell_item())
-# print(p.examine_item('tiny treasure'))
-# print(p.examine_player('player59'))
+# print(f"{p.move('w')}")
 
-# print(f"{p.take_item()}")
+# print(p.sell_item())
+# print(p.examine_item('great treasure'))
+# print(p.examine_player('shiny treasure'))
+# print(f"{p.take_item('small treasure')}")
+# print(f"{p.drop_item('small treasure')}")
+
+# print(db.get_room_by_id('0'))
+
 # print(get_status())
+# print(f"{p.wise_explore('n', 10)}")
+# explore(p, db, db_id)
 
 
 @app.route('/')
@@ -51,5 +66,5 @@ def player_check():
     return f"{res}"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
